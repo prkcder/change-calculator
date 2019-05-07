83c29afca395ec28a0948477178b7ee134bac3ba
John Park <jhnprk12@gmail.com>
2019-05-07 11:04:03 -0700

    all tests are passing

---
+++ README.md
@@ -0,0 +1,9 @@
+This is a Change Calculator.
+
+Download the folder, then run npm install
+
+start up the local server by typing in npm start
+
+Go to the local server website
+
+Input the sale amount and the amount received and press calculate to get the exact change
--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // Write your JavaScript here
+function clickChaChing() {
+    console.log('clicked');
+    
+    
+    var due = document.getElementById('amount-due').value * 100;
+    
+    var received = document.getElementById('amount-received').value * 100;
+    
+    var total = (received) - (due); 
+    
+    console.log(due, received, total);
+    
+    var Ds = Math.floor((total)/100);
+    document.getElementById('dollars-output').innerHTML = Ds;
+    
+    var Qa = Math.floor(((total)-(Ds*100))/25);
+    document.getElementById('quarters-output').innerHTML = Qa;
+    
+    var Di = Math.floor(((total)-(Ds*100)-(Qa*25))/10);
+    document.getElementById('dimes-output').innerHTML = Di;
+    
+    var Ni =  Math.floor(((total)-(Ds*100)-(Qa*25)-(Di*10))/5);
+    document.getElementById('nickels-output').innerHTML = Ni;
+    
+    var Pe =  Math.ceil(((total)-(Ds*100)-(Qa*25)-(Di*10)-(Ni*5))/1);
+    document.getElementById('pennies-output').innerHTML = Pe;
+    
+}
